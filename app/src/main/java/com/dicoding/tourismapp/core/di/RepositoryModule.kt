4e202f0d951c7dbda819b37a7ff2878114feed67
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-27 17:39:47 +0700

    based object provided

---
+++ app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
@@ -0,0 +1,15 @@
+package com.dicoding.tourismapp.core.di
+
+import com.dicoding.tourismapp.core.data.TourismRepository
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+import dagger.Binds
+import dagger.Module
+
+/** kode berikut yang digunakan untuk mem-provide interface dengan implementasinya. */
+@Module(includes = [NetworkModule::class, DatabaseModule::class])
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun provideRepository(tourismRepository: TourismRepository): ITourismRepository
+
+}
