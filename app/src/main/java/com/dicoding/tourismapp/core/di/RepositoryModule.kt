7126967ad75d87c87ef62d1ae07083c1216c4a3d
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-28 21:54:41 +0700

    add parcelize and module

--- app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
@@ -7,8 +7,7 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
-/** menambahkan annotation @InstallIn pada masing-masing module yang terdapat pada core dengan value ApplicationComponent */
-@Module
+@Module(includes = [NetworkModule::class, DatabaseModule::class])
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
