1c8d278f4234d52b3c190553e79daaf2d621d077
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-28 09:58:43 +0700

    add dataMapper function and changes constructor to usecase in viewModel

--- app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
@@ -1,11 +1,10 @@
 package com.dicoding.tourismapp.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
-class HomeViewModel(tourismRepository: TourismUseCase) : ViewModel() {
-
-    val tourism = tourismRepository.getAllTourism()
-
+class HomeViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
+    val tourism = tourismUseCase.getAllTourism().asLiveData()
 }
 
